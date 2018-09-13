52e96f47148927e16f76ccf413e76de9efbb0ff8
Josep Viciana <emmgfx@gmail.com>
2018-09-13 20:48:54 +0200

    Preparing project

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var clean = require('gulp-clean');
+var zip = require('gulp-zip');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', function() {
+    gulp.watch('sass/**/*.scss', ['styles']);
+});
+
+gulp.task('clean', function () {
+    return gulp.src('build', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', ['clean'], function () {
+    return gulp.src([
+        '**/*',
+        '!build/**/*',
+        '!gulpfile.js',
+        '!package-lock.json',
+        '!package.json',
+        '!sass/**/*',
+        '!sass',
+        '!node_modules/**/*',
+        '!node_modules',
+    ])
+    .pipe(zip('mrmt.zip'))
+    .pipe(gulp.dest('build'));
+});
