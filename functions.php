64af56eaa0b1223c320d7d64c0630c7f1f75a4d3
Josep Viciana <emmgfx@gmail.com>
2018-10-14 18:59:46 +0200

    Title and false content

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
 
 require_once 'includes/menus.php';
 
+add_action('after_setup_theme', 'theme_slug_setup');
+function theme_slug_setup() {
+    add_theme_support('title-tag');
+}
 
 wp_enqueue_script('jquery');
 wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array('jquery'), '4.1.3', true);
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
-<h1>Hello, world!</h1>
-<button class="btn btn-sm btn-primary">test</button>
+<div class="container">
+    
+</div>
 
 <?php get_footer(); ?>
