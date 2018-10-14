207038d262e8a6ff412440a570ee5657c7ba64f3
Josep Viciana <emmgfx@gmail.com>
2018-10-14 13:20:47 +0200

    Add bootstrap

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-test
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,500" rel="stylesheet">
+    <title>Hello, world!</title>
+</head>
+<body>
+    <h1>Hello, world!</h1>
+    <button class="btn btn-sm btn-primary">test</button>
+</body>
+</html>
