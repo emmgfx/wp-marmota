accd6062fb47cffc2dc453ab16cf8686f4a4c53e
Josep Viciana <emmgfx@gmail.com>
2018-10-14 17:10:25 +0200

    Header and footer

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,500" rel="stylesheet">
+    <title>Hello, world!</title>
+    <?php wp_head(); ?>
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
-    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,500" rel="stylesheet">
-    <title>Hello, world!</title>
-</head>
-<body>
-    <h1>Hello, world!</h1>
-    <button class="btn btn-sm btn-primary">test</button>
-</body>
-</html>
+<?php get_header(); ?>
+
+<h1>Hello, world!</h1>
+<button class="btn btn-sm btn-primary">test</button>
+
+<?php get_footer(); ?>
