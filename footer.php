16fc49e7278b70bb474776a9506d8c698865ef96
Josep Viciana <emmgfx@gmail.com>
2018-10-16 21:36:53 +0200

    Footer base content

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
     </main> <!-- main#content -->
     
     <footer>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-4">
+                    <a class="footer-brand" href="<?php echo get_home_url(); ?>"><?php echo get_bloginfo('name'); ?></a>
+                </div>
+                <div class="col-4 text-center">
+                    Social networks
+                </div>
+                <div class="col-4 text-right">
+                    Legal links
+                </div>
+            </div>
+        </div>
     </footer>
     
     <?php wp_footer(); ?>
