bf903abc709946a000a9c3ad28a87f5994f59a76
Josep Viciana <emmgfx@gmail.com>
2018-10-17 21:02:08 +0200

    Social links (and his settings)

--- footer.php
+++ footer.php
@@ -7,7 +7,12 @@
                     <a class="footer-brand" href="<?php echo get_home_url(); ?>"><?php echo get_bloginfo('name'); ?></a>
                 </div>
                 <div class="col-4 text-center">
-                    Social networks
+                    <ul class="list-unstyled list-inline m-0">
+                        <?php foreach( marmota_get_social_networks() as $slug => $label ): ?>
+                        <?php $url = esc_url(get_theme_mod($slug)); if(empty($url)) continue; ?>
+                        <li class="list-inline-item"><a href="<?php echo $url; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social/<?php echo str_replace('social_', '', $slug); ?>.svg" /></a></li>
+                        <?php endforeach; ?>
+                    </ul>
                 </div>
                 <div class="col-4 text-right">
                     Legal links
--- functions.php
+++ functions.php
@@ -9,3 +9,59 @@ function theme_slug_setup() {
 
 wp_enqueue_script('jquery');
 wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array('jquery'), '4.1.3', true);
+
+function marmota_get_social_networks(){
+    return array(
+        'social_dribbble' => __('Dribbble', 'marmota'),
+        'social_facebook' => __('Facebook', 'marmota'),
+        'social_instagram' => __('Instagram', 'marmota'),
+        'social_linkedin' => __('Linked In', 'marmota'),
+        'social_pinterest' => __('Pinterest', 'marmota'),
+        'social_telegram' => __('Telegram', 'marmota'),
+        'social_tumblr' => __('Telegram', 'marmota'),
+        'social_tumblr' => __('Tumblr', 'marmota'),
+        'social_twitch' => __('Twitch', 'marmota'),
+        'social_twitter' => __('Twitter', 'marmota'),
+        'social_youtube' => __('Youtube', 'marmota'),
+    );
+}
+    
+function customize_register( $wp_customize ) {
+
+    $wp_customize->add_panel( 'marmota', array(
+        'priority' => 160,
+        'capability' => 'edit_theme_options',
+        'title' => __( 'Marmota', 'marmota' ),
+        'description' => __( 'Ajustes de la plantilla', 'marmota' ),
+    ) );
+    
+    $wp_customize->add_section( 'social', array(
+        'title' => __( 'Redes sociales', 'marmota' ),
+        'description' => __( 'Recuerda incluir la URL completa, incluyendo el <code>https://</code>', 'marmota' ),
+        'panel' => 'marmota',
+        'priority' => 160,
+        'capability' => 'edit_theme_options',
+    ) );
+    
+    foreach( marmota_get_social_networks() as $slug => $label ) {
+        $wp_customize->add_setting( $slug, array(
+            'type' => 'theme_mod', // or 'option'
+            'capability' => 'edit_theme_options',
+            'theme_supports' => '', // Rarely needed.
+            'default' => '', // Ej: #000000
+            'transport' => 'refresh', // or postMessage
+            'sanitize_callback' => '', // Ej: 'sanitize_hex_color'
+            'sanitize_js_callback' => '', // Basically to_json.
+        ) );
+        
+        $wp_customize->add_control( $slug, array(
+            'type' => 'url',
+            'priority' => 10, // Within the section.
+            'section' => 'social', // Required, core or custom.
+            'label' => $label,
+            'description' => __( 'URL del perfil de ' . $label, 'marmota' ),
+        ) );
+    }
+
+}
+add_action( 'customize_register', 'customize_register' );
