41e5a8ecde8e160b6154d1a5f2c953c20b7d910d
Josep Viciana <emmgfx@gmail.com>
2018-10-21 13:00:10 +0200

    Comments context linked

--- context-detail.php
+++ context-detail.php
@@ -1,5 +1,5 @@
 <ul class="context list-inline list-unstyled">
     <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/calendar.svg" /> <?php the_date(); ?></li>
-    <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/comments.svg" /> <?php comments_number(); ?></li>
+    <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/comments.svg" /> <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></li>
     <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/tags.svg" /> <?php echo get_the_tag_list( '', ', ', '.' ); ?></li>
 </ul>
