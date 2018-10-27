1e49754adeab9a0b7c3b87c614162d3531ac646a
Josep Viciana <emmgfx@gmail.com>
2018-10-27 19:15:45 +0200

    Add support for post-thumbnails

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 require_once 'includes/menus.php';
 
+add_theme_support( "post-thumbnails" );
+
 add_action('after_setup_theme', 'theme_slug_setup');
 function theme_slug_setup() {
     add_theme_support('title-tag');
