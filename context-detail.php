c869d33f0ebf79bcacf8c333e596d0114fc1ac97
Josep Viciana <emmgfx@gmail.com>
2018-10-28 13:22:20 +0100

    Don't show empty tags list

--- context-detail.php
+++ context-detail.php
@@ -1,5 +1,12 @@
+<?php
+$tags_list = get_the_tag_list( '', ', ', '.' );
+?>
 <ul class="context list-inline list-unstyled">
     <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/calendar.svg" /> <?php the_date(); ?></li>
     <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/comments.svg" /> <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></li>
-    <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/tags.svg" /> <?php echo get_the_tag_list( '', ', ', '.' ); ?></li>
+    <?php if( $tags_list ): ?>
+    <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/tags.svg" /> <?php echo $tags_list; ?></li>
+    <?php else: ?>
+    <li class="list-inline-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/tags.svg" /> <?php echo __("Sin etiquetas", "marmota"); ?></li>
+    <?php endif; ?>
 </ul>
