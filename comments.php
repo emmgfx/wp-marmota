76ce8426c8795e9bd720e2ac9e2891f2df3a6cb4
Josep Viciana <emmgfx@gmail.com>
2018-10-28 15:15:04 +0100

    Comments pagination

--- comments.php
+++ comments.php
@@ -16,11 +16,19 @@
 						'format' => 'html5',
 					));
 					?>
-					<div class="navigation">
+					<nav class="comments-navigation">
 						<?php
-						paginate_comments_links( array( 'type' => 'list' ) );
+						paginate_comments_links(array(
+                            'prev_text' => '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="#C1BFBF" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+                            </svg>',
+                            'next_text' => '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="#C1BFBF" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                            </svg>',
+                            'mid_size' => 2
+                        ));
 						?>
-				    </div>
+				    </nav>
 			    </div>
 			<?php else : ?>
 			    <p><?php echo __('No hay comentarios. Sé el primero en comentar.', 'marmota'); ?></p>
