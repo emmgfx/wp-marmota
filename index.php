af51d5f941005ca27ae97cfa2a1c390a9bdb3c5e
Josep Viciana <emmgfx@gmail.com>
2018-12-15 21:04:47 +0100

    Posts pagination in template part

--- index.php
+++ index.php
@@ -10,17 +10,7 @@
     }
     ?>
     
-    <?php
-    the_posts_pagination(array(
-        'prev_text' => '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
-            <path fill="#C1BFBF" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
-        </svg>',
-        'next_text' => '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
-            <path fill="#C1BFBF" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-        </svg>',
-        'mid_size' => 2
-    ));
-    ?>
+    <?php get_template_part('pagination', 'posts'); ?>
     
 </div>
 
