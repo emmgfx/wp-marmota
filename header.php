6d91433a1f4d97781b3998854803cc50d890a2e2
Josep Viciana <emmgfx@gmail.com>
2019-02-23 21:19:27 +0100

    Sepparate search forms

--- header.php
+++ header.php
@@ -38,28 +38,13 @@
                             <path fill="#FFFFFF" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
                         </svg>
                     </button>
-                    <form class="search form-inline my-3 my-<?php echo esc_attr(get_theme_mod("navbar-expand", "md")); ?>-0 d-<?php echo esc_attr(get_theme_mod("navbar-expand", "md")); ?>-none" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-                        <div class="d-flex w-100">
-                            <input class="form-control mr-2 flex-grow-1" type="search" placeholder="<?php echo __("Buscar", "marmota"); ?>" value="<?php echo get_search_query() ?>" name="s" aria-label="<?php echo __("Buscar", "marmota"); ?>">
-                            <button class="btn btn-primary btn-sm" type="submit"><?php echo __("Buscar", "marmota"); ?></button>
-                        </div>
-                    </form>
+                    <?php get_search_form(); ?>
                 </div>
                 
             </di>
         </nav>
         
-        <section class="desktop-search">
-            <form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-                <input type="search" placeholder="<?php echo __("Buscar", "marmota"); ?>" value="<?php echo get_search_query() ?>" name="s" aria-label="<?php echo __("Buscar", "marmota"); ?>">
-                <button class="close-search" type="button">
-                    ESC
-                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                        <path fill="#FFFFFF" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-                    </svg>
-                </button>
-            </form>
-        </section>
+        <?php get_template_part('searchform', 'desktop'); ?>
         
     </header>
     
---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form class="search form-inline my-3 my-<?php echo esc_attr(get_theme_mod("navbar-expand", "md")); ?>-0 d-<?php echo esc_attr(get_theme_mod("navbar-expand", "md")); ?>-none" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+    <div class="d-flex w-100">
+        <input class="form-control mr-2 flex-grow-1" type="search" placeholder="<?php echo __("Buscar", "marmota"); ?>" value="<?php echo get_search_query() ?>" name="s" aria-label="<?php echo __("Buscar", "marmota"); ?>">
+        <button class="btn btn-primary btn-sm" type="submit"><?php echo __("Buscar", "marmota"); ?></button>
+    </div>
+</form>
