692bcaa9cdb0ed3aeab47ecb4403ae94f26228e9
Josep Viciana <emmgfx@gmail.com>
2019-03-24 13:58:04 +0100

    Correct pagination with sticky posts

--- functions.php
+++ functions.php
@@ -8,21 +8,50 @@ require_once 'includes/sidebars.php';
 function marmota_current_page(){
     return get_query_var('paged') ?: 1;
 }
+
 function marmota_is_first_page(){
     return marmota_current_page() == 1;
 }
 
+function marmota_has_sticky_post(){
+    $sticky = get_option( 'sticky_posts' );
+    return isset($sticky[0]);
+}
+
 function modify_default_query( $query ) {
+    
+    # This function modifies the post_per_page setting, in order to
+    # allow a correct pagination and the use of a featured last post or
+    # sticky post.
+    #
+    # If the user is in the first page of the blog, checks if exists any sticky
+    # post. The result is: If the user is in the first page and don't exists any
+    # sticky post, add one post to the posts_per_page in the query. And to the
+    # the same in other pages, but adding one to the offset instead of
+    # posts_per_page.
     
     if( $query->is_home() ):
         
         $posts_per_page = get_option('posts_per_page');
-        
-        if( $query->get('paged') == 0 ):
-            $query->set('posts_per_page', $posts_per_page + 1);
+
+        if( marmota_is_first_page() ):
+            
+            if( !marmota_has_sticky_post() ){
+                $posts_per_page++;
+            }
+    
+            $query->set('posts_per_page', $posts_per_page);
+            
         else:
-            $first_post_of_this_page = $posts_per_page * $query->get('paged') - $posts_per_page;
-            $query->set('offset', $first_post_of_this_page + 1 );
+            
+            $offset = $posts_per_page * $query->get('paged') - $posts_per_page;
+            
+            if( !marmota_has_sticky_post() ){
+                $offset++;
+            }
+            
+            $query->set('offset', $offset );
+            
         endif;
     
     endif;
