74fbeea02ed857bec69e3e9c66679dd4edc8ac42
Josep Viciana <emmgfx@gmail.com>
2019-03-24 16:12:21 +0100

    Improve above the footer widgets responsive and styles

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -6,7 +6,7 @@ function marmota_sidebars() {
 		'class'         => 'row',
 		'name'          => __( 'Sobre el pie de p&aacute;gina', 'marmota' ),
 		'description'   => __( 'Widgets sobre el pie de p&aacute;gina', 'marmota' ),
-		'before_widget' => '<div class="col">',
+		'before_widget' => '<div class="col-12 col-sm-6 col-md-4 mb-5">',
 		'after_widget'  => '</div>',
 	);
 	register_sidebar( $args );
