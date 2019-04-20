662d2b00bc37b14e97445912363c62333dab184a
Josep Viciana <emmgfx@gmail.com>
2019-04-20 15:21:33 +0200

    Bug on the lists context dates

--- context-list.php
+++ context-list.php
@@ -1,4 +1,4 @@
 <ul class="context list-inline list-unstyled">
-    <li class="list-inline-item"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/icons/calendar.svg" /> <?php the_date(); ?></li>
+    <li class="list-inline-item"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/icons/calendar.svg" /> <?php echo get_the_date(); ?></li>
     <li class="list-inline-item"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/icons/comments.svg" /> <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></li>
 </ul>
