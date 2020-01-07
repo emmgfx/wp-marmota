04fc7d8dab1a0ac135193a1a35b59193ee95b663
Josep Viciana <emmgfx@gmail.com>
2020-01-07 09:50:39 +0100

    Small correction for pagination

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="container">
     
     <?php
-    if(marmota_is_first_page()){
+    if(marmota_is_first_page() && is_home()){
         get_template_part('article_list', 'first_page');
     }else{
         get_template_part('article_list');
